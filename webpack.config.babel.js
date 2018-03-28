6ceafc0f13ecb693cac455235b90c20918811189
GrandCatsWineGrapes <grandcatswinegrapes@gmail.com>
2018-03-28 15:50:38 +0300

    Test for windows

---
+++ webpack.config.babel.js
@@ -0,0 +1,62 @@
+import path from 'path';
+
+const config = {
+    entry: './client/src/index.js',
+    resolve: {
+        extensions: ['.js']
+    },
+    output: {
+        path: path.resolve('./', 'public'),
+        filename: 'app.bundle.js'
+    },
+    
+  module: {
+        /*rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'babel-loader',
+                options: {
+                  "presets": [["es2015", {"modules":false}], "react"]
+                }
+              }
+            ]
+          }
+        ],*/
+        loaders: [
+        {
+          test: /\.(json)$/,
+          exclude: /node_modules/,
+          loader: 'json-loader',
+        },
+        {
+          test: /\.(js|jsx)$/,
+          loader: 'babel-loader',
+          options: {
+            "presets": [["es2015", {"modules":false}], "react"]
+          }
+      
+        },
+        {
+          test: /\.(?:png|jpg|gif|svg)$/,
+          loader: 'url-loader',
+        },
+        {
+          test: /\.css/,
+          loader: 'style-loader!css-loader?localIdentName=[local]-[hash:base64:5]',
+        },
+        {
+          test: /\.less/,
+          loader: 'style-loader!css-loader?modules&localIdentName=[local]-[hash:base64:5]!less',
+        },
+      ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
+
+export default config;
