96d395304a8a6228283caaa2cf82e7473866215c
Premor <spamerkik@gmail.com>
2018-04-01 14:49:49 +0300

    add

--- themes/default/index.js
+++ themes/default/index.js
@@ -1,5 +1,5 @@
 exports.install = function() {
-	F.merge('/default/css/default.css', '/css/ui.css', '/css/bootstrap.min.css', '=default/public/css/default.css');
+	F.merge('/default/css/default.css', '/css/ui.css','/css/for_pages/news.css', '/css/for_pages/Header.css', '/css/bootstrap.min.css', '=default/public/css/default.css');
 	F.merge('/default/js/default.js', '/js/jctajr.min.js', '/js/ui.js', '=default/public/js/default.js');
 	F.localize('/default/templates/*.html', ['compress']);
 };
