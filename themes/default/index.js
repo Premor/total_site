ae262b1f5b05bde4ea4fd7697d5cf6e2017283bd
GrandCatsWineGrapes <grandcatswinegrapes@gmail.com>
2018-04-15 16:03:03 +0300

    Bugs fixed

--- themes/default/index.js
+++ themes/default/index.js
@@ -9,9 +9,9 @@ exports.install = function() {
 	 '/css/ui.css', '/css/bootstrap.min.css', '/css/animate.css',
 	 '=default/public/css/default.css');
 	F.merge('/default/js/default.js', '/js/jctajr.min.js', '/js/ui.js', 
-	/*cheks if in vp*/ '/js/jquery.viewportchecker.min.js',
-	/*WowJS*/ '/js/wow.min.js',
-	/*my main .js file*/ '/js/main.js',
+	/*cheks if in vp '/js/jquery.viewportchecker.min.js',*/
+	/*WowJS '/js/wow.min.js',*/
+	/*my main .js file '/js/main.js',*/
 	 '=default/public/js/default.js');
 	F.localize('/default/templates/*.html', ['compress']);
 };
