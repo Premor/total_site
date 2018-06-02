22fcb52ee29f4a8493e9647ee85536b3e2c5914d
GrandCatsWineGrapes <grandcatswinegrapes@gmail.com>
2018-06-02 19:12:27 +0300

    ADDED FIRST ANIMATIONS I FEEL LIKE A GOD

--- public/js/common.js
+++ public/js/common.js
@@ -12,4 +12,108 @@ $(document).ready(function() {
     $('.port_law').on('click',function(){
         window.location.replace('/practice?practice=law')
     })
+
+
+    /* For practice animation */
+    $('.fiz2_block').on('click',function(){
+        $('.pr_page').css('position','relative');
+        $('.pr_page').css('z-index','0');
+        $(this).css('z-index','50')
+        $(this).animate({left: '-15%'},700);
+        
+        $('.port2_block').animate({left: '-31.5%'},700);
+        setTimeout(() => {
+            $('.port2_block').css('visibility','hidden');
+        }, 700);
+
+        $('.doc2_block').animate({left: '-46.5%'},700);
+        setTimeout(() => {
+            $('.doc2_block').css('visibility','hidden');
+        }, 700);
+
+        $('.law2_block').animate({left: '-65.5%'},700);
+        setTimeout(() => {
+            $('.law2_block').css('visibility','hidden');
+        }, 700);
+    });
+
+    $('.port2_block').on('click',function(){
+        $('.pr_page').css('position','relative');
+        $('.pr_page').css('z-index','0');
+        $(this).css('z-index','50')
+        $(this).animate({left: '-31.5%'},700);
+        
+        $('.fiz2_block').animate({left: '-15%'},700);
+        setTimeout(() => {
+            $('.fiz2_block').css('visibility','hidden');
+        }, 700);
+
+        $('.doc2_block').animate({left: '-46.5%'},700);
+        setTimeout(() => {
+            $('.doc2_block').css('visibility','hidden');
+        }, 700);
+
+        $('.law2_block').animate({left: '-65.5%'},700);
+        setTimeout(() => {
+            $('.law2_block').css('visibility','hidden');
+        }, 700);
+
+        // setTimeout(() => {
+        //     $('.port2_block').css('left','-15%');
+        // }, 700);
+    });
+
+    $('.doc2_block').on('click',function(){
+        $('.pr_page').css('position','relative');
+        $('.pr_page').css('z-index','0');
+        $(this).css('z-index','50')
+        $(this).animate({left: '-48.5%'},7000);
+        
+        $('.port2_block').animate({left: '-31.5%'},700);
+        setTimeout(() => {
+            $('.port2_block').css('visibility','hidden');
+        }, 700);
+
+        $('.fiz2_block').animate({left: '-15%'},700);
+        setTimeout(() => {
+            $('.fiz2_block').css('visibility','hidden');
+        }, 700);
+
+        $('.law2_block').animate({left: '-65.5%'},700);
+        setTimeout(() => {
+            $('.law2_block').css('visibility','hidden');
+        }, 700);
+
+      /*  setTimeout(() => {
+            $('.doc2_block').css('left','-15%');
+        }, 700);*/
+    });
+
+    $('.law2_block').on('click',function(){
+        $('.pr_page').css('position','relative');
+        $('.pr_page').css('z-index','0');
+        $(this).css('z-index','50')
+        $(this).animate({left: '-64.5%'},700);
+        
+        $('.port2_block').animate({left: '-31.5%'},700);
+        setTimeout(() => {
+            $('.port2_block').css('visibility','hidden');
+        }, 700);
+
+        $('.doc2_block').animate({left: '-46.5%'},700);
+        setTimeout(() => {
+            $('.doc2_block').css('visibility','hidden');
+        }, 700);
+
+        $('.fiz2_block').animate({left: '-15%'},700);
+        setTimeout(() => {
+            $('.fiz2_block').css('visibility','hidden');
+        }, 700);
+
+        /*setTimeout(() => {
+            $('.law2_block').css('left','-15%');
+        }, 707);*/
+    });
+
+
 })
